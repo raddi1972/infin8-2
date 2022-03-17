460f429eed6b9edee4b89bdcfdc73337b04dc49b
raddi1972 <dixitrudransh01@gmail.com>
2022-03-17 16:31:16 +0530

    initial

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let header = document.querySelector('header');
+let lastScrollY = window.scrollY;
+let video = document.querySelector('#myVideo');
+
+window.addEventListener('scroll', () => {
+    if (lastScrollY < window.scrollY) {
+        header.classList.add('nav-hidden');
+
+    } else {
+        header.classList.remove('nav-hidden');
+    }
+    lastScrollY = window.scrollY
+
+
+    header.classList.toggle('site-header-active', window.scrollY > 0);
+});
